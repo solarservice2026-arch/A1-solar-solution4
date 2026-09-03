b7a0e91fbdc3697d20fecd8a557c9d14ecc8b32f
Master Ayush Gautam <ayushgautam2007.kg@gmail.com>
2026-09-03 23:05:05 +0530

    fix(formatting): add newline for better readability in download_u2netp.py

--- download_u2netp.py
+++ download_u2netp.py
@@ -3,6 +3,7 @@ import requests
 
 url = "https://github.com/danielgatis/rembg/releases/download/v0.0.0/u2netp.onnx"
 home_dir = os.path.expanduser("~")
+
 u2net_dir = os.path.join(home_dir, ".u2net")
 os.makedirs(u2net_dir, exist_ok=True)
 model_path = os.path.join(u2net_dir, "u2netp.onnx")
